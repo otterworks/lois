3ae7c157386db9622bedc2f8af9500dae88fb1d0
M J Stanway <m.j.stanway@alum.mit.edu>
2022-03-05 09:49:57 -0500

    add __main__.py at module level

---
+++ src/lois/__main__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from .spy.spy import LOIS
+
+def main(channel, scale=0.5, verbose=0):
+    """Show images"""
+    s = LOIS([channel], scale=scale, verbose=verbose)
+    s.spy()
+
+
+if __name__ == "__main__":
+    import argparse
+    P = argparse.ArgumentParser(description="LCM OpenCV Image Spy")
+    P.add_argument('-s', '--scale', default=0.5,
+                   help='display verbose output')
+    P.add_argument('-v', '--verbose', action='count', default=0,
+                   help='display verbose output')
+    P.add_argument('-V', '--version', action='version',
+                   version='%(prog)s 0.0.1',
+                   help='display version information and exit')
+    P.add_argument('channel', help='the LCM channel to listen on')
+    A = P.parse_args()
+    main(**A.__dict__)
